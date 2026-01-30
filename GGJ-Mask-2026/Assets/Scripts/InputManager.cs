211d25119040d168abb64ef0a44b9bebfc778e72
dingo <141416442+zwazoman@users.noreply.github.com>
2026-01-30 22:04:06 +0100

    afazfaf

---
+++ GGJ-Mask-2026/Assets/Scripts/InputManager.cs
@@ -0,0 +1,43 @@
+using System.Xml.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InputManager : MonoBehaviour
+{
+    #region Singleton
+    private static InputManager instance;
+
+    public static InputManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                GameObject go = new GameObject("InputManager");
+                instance = go.AddComponent<InputManager>();
+            }
+            return instance;
+        }
+    }
+
+    #endregion
+
+    public UnityEvent<Vector2> OnLeftClick;
+    public UnityEvent<Vector2> OnRightClick;
+
+    private void Awake()
+    {
+        if (instance == null || instance == this)
+            instance = this;
+        else
+            Destroy(this);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+            OnLeftClick?.Invoke(Input.mousePosition);
+        if(Input.GetKeyDown(KeyCode.Mouse1))
+            OnRightClick?.Invoke(Input.mousePosition);
+    }
+}
