afcbc2ed806c08c5f8c0ac7966e378b7f259520e
dingo <141416442+zwazoman@users.noreply.github.com>
2026-01-31 14:01:09 +0100

    uu

--- GGJ-Mask-2026/Assets/Scripts/Animals/Fish.cs
+++ GGJ-Mask-2026/Assets/Scripts/Animals/Fish.cs
@@ -5,6 +5,7 @@ public class Fish : Animal
     [SerializeField] Rigidbody2D _rb;
 
     [SerializeField] float _jumpStrength = 2;
+    [SerializeField] float _torqueStrength = 5;
 
 
     protected override void Awake()
@@ -21,6 +22,7 @@ public class Fish : Animal
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             _rb.AddForce(transform.up * _jumpStrength, ForceMode2D.Impulse);
+            _rb.AddTorque(_torqueStrength, ForceMode2D.Impulse);
         }
     }
 
