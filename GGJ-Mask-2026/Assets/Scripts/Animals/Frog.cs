920b738f63e1af3b6eb315644a8af335924f8482
nathan-exe <nathan.tazi.1@gmail.com>
2026-01-31 10:24:09 +0100

    frog

---
+++ GGJ-Mask-2026/Assets/Scripts/Animals/Frog.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Frog : Controllable
+{
+    public override void GainControl()
+    {
+        base.GainControl();
+    }
+
+    public override void LooseControl()
+    {
+        base.LooseControl();
+    }
+}
