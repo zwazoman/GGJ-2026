3e91bb14ba9b06f02df91993c2d0580e2ac3e449
dingo <141416442+zwazoman@users.noreply.github.com>
2026-01-31 20:18:18 +0100

    je me casse bonne nuit a tous ceux qui travaillent et les autres

--- GGJ-Mask-2026/Assets/Scripts/Animals/Visuals/MoleVisuals.cs
+++ GGJ-Mask-2026/Assets/Scripts/Animals/Visuals/MoleVisuals.cs
@@ -1,6 +1,32 @@
+using System.Globalization;
 using UnityEngine;
 
 public class MoleVisuals : AnimalVisual
 {
+    [SerializeField] Animator _animator;
 
+    Mole _mole;
+
+    private void Awake()
+    {
+        _mole = _animal as Mole;
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+
+        _mole.OnDig += Dig;
+        _mole.OnReach += Reach;
+    }
+
+    void Dig()
+    {
+        _animator.SetBool("isDigging", true);
+    }
+
+    void Reach()
+    {
+        _animator.SetBool("isDigging", false);
+    }
 }
