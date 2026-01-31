09db0de4d577b0c03a8d666c45e60994e2e49a6d
dingo <141416442+zwazoman@users.noreply.github.com>
2026-01-31 15:18:18 +0100

    chépo

--- GGJ-Mask-2026/Assets/Scripts/Animals/Fish.cs
+++ GGJ-Mask-2026/Assets/Scripts/Animals/Fish.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 public class Fish : Animal
 {
     [SerializeField] Rigidbody2D _rb;
+    [SerializeField] LayerMask _layerMask;
 
     [SerializeField] float _jumpStrength = 2;
-    [SerializeField] float _torqueStrength = 5;
+    [SerializeField] float _torqueStrength = 1;
+    [SerializeField] float _groundedHeight = 1;
 
+    bool _isGrounded = true;
 
     protected override void Awake()
     {
@@ -19,8 +22,17 @@ public class Fish : Animal
     {
         base.Update();
 
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (!isControlled)
+            return;
+
+        if (Physics2D.Raycast(transform.position, Vector2.down, _groundedHeight, _layerMask))
+            _isGrounded = true;
+        else
+            _isGrounded = false;
+
+        if (Input.GetKeyDown(KeyCode.Mouse0) && _isGrounded)
         {
+            print("frétille");
             _rb.AddForce(transform.up * _jumpStrength, ForceMode2D.Impulse);
             _rb.AddTorque(_torqueStrength, ForceMode2D.Impulse);
         }
