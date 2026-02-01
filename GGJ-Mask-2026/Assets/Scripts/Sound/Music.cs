32bf7269456edd11db5b9136a8916e169d605a87
zwazoman <gloupik.lemaire@gmail.com>
2026-02-01 19:36:24 +0100

    fix final

--- GGJ-Mask-2026/Assets/Scripts/Sound/Music.cs
+++ GGJ-Mask-2026/Assets/Scripts/Sound/Music.cs
@@ -2,8 +2,13 @@ using UnityEngine;
 
 public class Music : MonoBehaviour
 {
+    static bool exists;
     private void Awake()
     {
+        if(exists)
+            Destroy(gameObject);
+
         DontDestroyOnLoad(gameObject);
+        exists = true;
     }
 }
